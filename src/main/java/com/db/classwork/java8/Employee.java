672fc86282592ebebaa382c1601ffeff97865036
Irina Tsekhanovich <irina.tsekhanovich@softline.com>
2021-08-24 11:52:47 +0300

    count of words

---
+++ src/main/java/com/db/classwork/java8/Employee.java
@@ -0,0 +1,19 @@
+package com.db.classwork.java8;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * TODO Class Description
+ *
+ * @author IrinaTsekhanovich
+ * @since 24.08.2021
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+	private String name;
+	private int salary;
+}
--- src/main/java/com/db/classwork/java8/Main.java
+++ src/main/java/com/db/classwork/java8/Main.java
@@ -1,5 +1,11 @@
 package com.db.classwork.java8;
 
+import lombok.SneakyThrows;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -22,7 +28,17 @@ public class Main {
 		);
 	}
 
+	public static int salarySum(List<Employee> employees) {
+		return employees.stream().mapToInt(Employee::getSalary).sum();
+	}
+
+	@SneakyThrows
 	public static void main(String[] args) {
-		ForEachWithDelay(List.of(1,2,3,4,5), 1000, System.out::println);
+		Path path = Paths.get("data/song.txt");
+
+		long count = Files.lines(path).flatMap(s -> Arrays.stream(s.trim().split(" "))).count();
+		System.out.println(count);
+
+
 	}
 }
